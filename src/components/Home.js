f947e53dd735b68ae230c14b890df37eaaa4055b
saw <saw>
2025-06-09 23:10:26 +1200

    Update name

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
               <h3 className="name">
                 <span>I</span> am
                 <br />
-                MuKaHbaw Aye
+                MuKaHbaw
               </h3>
               <span className="welcome">UI UX Designer / Frontend Developer</span>
               <div className="tonni_tm_button">
